ee0385ceb2678fa57ef2642f3f89ce1e95fdb8e0
tanboGithub <tanbo2529@163.com>
2019-02-18 10:03:07 +0800

    add obs driver

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -17,6 +17,7 @@ import (
 	_ "github.com/docker/distribution/registry/storage/driver/oss"
 	_ "github.com/docker/distribution/registry/storage/driver/s3-aws"
 	_ "github.com/docker/distribution/registry/storage/driver/swift"
+        _ "github.com/docker/distribution/registry/storage/driver/obs"
 )
 
 func main() {
